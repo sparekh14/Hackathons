30ed1cca49ded95496b272d48218483cb780f300
Samarth Parekh <parekh.samarth@gmail.com>
2023-09-05 16:32:13 -0400

    Add files via upload

---
+++ PennApps/Journal.swift
@@ -0,0 +1,19 @@
+//
+//  Journal.swift
+//  PennApps
+//
+//  Created by Aditya Shirodkar on 3/19/22.
+//
+
+import Foundation
+
+
+struct Journal : Identifiable {
+    var id : String
+    var username: String
+    var journalName: String
+    var wentWell : String
+    var didntGoWell : String
+    var workOn : String
+    var rating : Int
+}
